2262b03ede7841757e912dc2d7af34726df3725f
Ngô Văn Đông <68848203+ngovandong@users.noreply.github.com>
2022-06-21 14:29:33 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,8 @@ import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = "//b27a-118-69-61-78.ap.ngrok.io/"
-axios.defaults.headers.get['Host'] = "b27a-118-69-61-78.ap.ngrok.io"
-axios.defaults.headers.post['Host'] = "b27a-118-69-61-78.ap.ngrok.io"
-axios.defaults.headers.common['Host'] = "b27a-118-69-61-78.ap.ngrok.io"
+axios.defaults.baseURL = "//9c1d-118-69-61-78.ap.ngrok.io/"
+
 new Vue({
   router,
   store,
