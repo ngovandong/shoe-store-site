4b35d420b3b21f872c204dcc8b7baba5bdf20fd4
Ngô Văn Đông <68848203+ngovandong@users.noreply.github.com>
2022-06-21 11:28:02 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = "https://b79f-203-205-51-183.ap.ngrok.io/"
+axios.defaults.baseURL = "//b79f-203-205-51-183.ap.ngrok.io/"
 new Vue({
   router,
   store,
